a28cdc85b268ec5c5f7c1381024cd299ea218974
daheeahn <deg9810@gmail.com>
2022-11-25 19:45:03 +0900

    [part2-ch6-10-커스텀 폰트 적용] 최종 코드

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import LottieView from 'lottie-react-native';
+import { useFonts } from 'expo-font';
 
 import Button from './src/Button';
 import { useCookie } from './src/use-cookie';
@@ -19,6 +20,9 @@ SplashScreen.preventAutoHideAsync();
 export default function App() {
   const { t, locale, setLocale, format } = useTranslation();
   const { cookieKey } = useCookie();
+  const [fontsLoaded] = useFonts({
+    'RIDIBatang': require('./assets/fonts/RIDIBatang.otf'), // https://ridicorp.com/ridibatang
+  });
 
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -41,10 +45,10 @@ export default function App() {
     }
   }, [cookieKey]);
   useEffect(() => {
-    if (locale !== null) {
+    if (locale !== null && fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [locale]);
+  }, [locale, fontsLoaded]);
  
   if (!isLoaded) return <LoadingView />;
 
@@ -93,12 +97,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   todayText: {
+    fontFamily: "RIDIBatang",
     position: "absolute",
     top: 70,
     fontSize: 13,
     color: "#8b658f",
   },
   cookieText: {
+    fontFamily: "RIDIBatang",
     fontSize: 22,
     color: "#372538",
     textAlign: "center",
